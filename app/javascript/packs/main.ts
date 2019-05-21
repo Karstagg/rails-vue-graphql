d17c47ac15591d97d270e7fa47967fa348081895
matthewFisher <m.r.f.48215@gmail.com>
2019-05-21 15:59:40 +0900

    Initial commit, boilerplate

---
+++ app/javascript/packs/main.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue/dist/vue.esm'
+
+document.addEventListener('DOMContentLoaded', () => {
+  let app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+})
