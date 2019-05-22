87f0b237523943c201dee0ce21e5d2de4d37bcc8
matthewFisher <m.r.f.48215@gmail.com>
2019-05-22 12:46:47 +0900

    adjusting graphql types

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -1,29 +1,16 @@
 module Mutations
   class SignInUser < BaseMutation
-    null true
-
-    argument :email, Types::AuthProviderEmailInput, required: false
-
-    field :token, String, null: true
-    field :user, Types::UserType, null: true
+    # ...code
 
     def resolve(email: nil)
-      # basic validation
-      return unless email
+      # ...code
 
-      user = User.find_by email: email[:email]
-
-      # ensures we have the correct user
-      return unless user
-      return unless user.authenticate(email[:password])
-
-      # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-
-      #todo replace with jwt and better encrytion
       token = crypt.encrypt_and_sign("user-id:#{ user.id }")
 
-      { user: user, token: token }
+      context[:session][:token] = token
+
+      # ...code
     end
   end
 end
